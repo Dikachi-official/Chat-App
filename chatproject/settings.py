fa6ff6b5239e80d4d21a2bb99665c90da90db386
Mike <nwokontadikachi@gmail.com>
2022-10-31 14:46:06 +0100

    deployment

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool) # TO CONVERT TO BOOLEAN
 
-ALLOWED_HOSTS = ['web-production-733f.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-b0ce.up.railway.app', '127.0.0.1']
 
 
 # Application definition
