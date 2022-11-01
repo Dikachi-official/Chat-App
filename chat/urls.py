b91ade97c953f1b0673b7f968bee9cf76da78f0e
Mike <nwokontadikachi@gmail.com>
2022-11-01 18:40:56 +0100

    more

--- chat/urls.py
+++ chat/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room>/', views.room, name='room'), #Dynamic url
-    path('assessview/', views.assessview, name='assessview'), # Url To assess the room being asked by user
+    path('<str:room>', views.room, name='room'), #Dynamic url
+    path('assessview', views.assessview, name='assessview'), # Url To assess the room being asked by user
     path('send', views.send, name='send'),  #In Line with our ajax "url", to add the functionality of sending the message
-    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),  #Get messages from a particular room (DYNAMIC URL)
+    path('getMessages/<str:room>', views.getMessages, name='getMessages'),  #Get messages from a particular room (DYNAMIC URL)
 ]
